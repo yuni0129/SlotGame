4708344612ff322430d71237a99aed7fdc0299e1
yuni0129 <150868172+yuni0129@users.noreply.github.com>
2024-01-19 11:49:43 +0900

    Add files via upload

--- Slot.java
+++ Slot.java
@@ -10,15 +10,15 @@ public class Slot {
 
         // 3回繰り返す
         for (int i = 0; i < 3; i++) {
-            System.out.println("enterを押してください");
+            System.out.println("数字を揃えましょう！enterを押してください");
             scanner.nextLine(); // Enterキーをクリア
             int playerChoice = new Random().nextInt(3) + 1;
-            System.out.println("数字：" + playerChoice);
+            System.out.println("あなたの数字：" + playerChoice);
             scanner.nextLine(); // Enterキーをクリア
 
             // プレイヤーがボタンを押すことで数字が決まる
             int computerChoice = new Random().nextInt(3) + 1; // 1から3のランダムな数字
-            System.out.println("数字：" + computerChoice);
+            System.out.println("コンピューターの数字：" + computerChoice);
 
             // すべて揃ったら勝ち、それ以外は負け（勝敗の決定）
             if (playerChoice == computerChoice) {
